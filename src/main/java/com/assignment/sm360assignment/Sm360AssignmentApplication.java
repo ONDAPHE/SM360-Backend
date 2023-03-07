e5af900960cd9ca020890f2b962c717e80440dbd
Ondaphe <arthurondaphe@gmail.com>
2023-03-07 21:53:46 +0400

    initialisation du projet

---
+++ src/main/java/com/assignment/sm360assignment/Sm360AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.sm360assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sm360AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sm360AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/sm360assignment/Sm360AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.sm360assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sm360AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
