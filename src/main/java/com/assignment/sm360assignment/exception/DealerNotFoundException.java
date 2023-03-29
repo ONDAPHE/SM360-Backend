e5c5ab3d84dc34c42c33dafa1bc2e8b062b89c63
Ondaphe <arthurondaphe@gmail.com>
2023-03-29 11:42:57 +0400

    MAJ

---
+++ src/main/java/com/assignment/sm360assignment/exception/DealerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.assignment.sm360assignment.exception;
+
+public class DealerNotFoundException extends RuntimeException{
+    public DealerNotFoundException(Integer dealerId) {
+        super(String.format("Dealer with id %s not found", dealerId));
+    }
+}
